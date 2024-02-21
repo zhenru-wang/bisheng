f90dd3005bf58e50b9c33a76cf847909dc8340c6
jingwangyaun <jingwangyuan@dataelem.com>
2024-02-21 16:45:19 +0800

    zhipuairag init

--- src/bisheng-langchain/bisheng_langchain/chat_models/__init__.py
+++ src/bisheng-langchain/bisheng_langchain/chat_models/__init__.py
@@ -5,8 +5,9 @@ from .qwen import ChatQWen
 from .wenxin import ChatWenxin
 from .xunfeiai import ChatXunfeiAI
 from .zhipuai import ChatZhipuAI
+from .zhipuai import ChatZhipuAIrag
 
 __all__ = [
     'ProxyChatLLM', 'ChatMinimaxAI', 'ChatWenxin', 'ChatZhipuAI', 'ChatXunfeiAI', 'HostChatGLM',
-    'HostBaichuanChat', 'HostLlama2Chat', 'HostQwenChat', 'CustomLLMChat', 'ChatQWen'
+    'HostBaichuanChat', 'HostLlama2Chat', 'HostQwenChat', 'CustomLLMChat', 'ChatQWen', 'ChatZhipuAIrag'
 ]
